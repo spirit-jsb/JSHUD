e254a03289b71069606bfb9d3a6776f1fe00ba5b
Max <sibo_jian_29903549@163.com>
2018-11-19 11:14:48 +0800

    [功能]($/JSProgressHUDStyle)
    初始化 JSProgressHUDStyle，负责管理有关 ProgressHUD 的样式信息。

---
+++ Sources/Core/JSProgressHUDStyle.swift
@@ -0,0 +1,31 @@
+//
+//  JSProgressHUDStyle.swift
+//  JSProgressHUD
+//
+//  Created by Max on 2018/11/19.
+//  Copyright © 2018 Max. All rights reserved.
+//
+
+import Foundation
+
+public enum JSProgressHUDMode {
+    case indeterminate
+    case determinate
+    case determinateHorizontalBar
+    case annularDeterminate
+    case customView
+    case text
+}
+
+public enum JSProgressHUDAnimation {
+    case fade
+    case zoom
+    case zoomOut
+    case zoomIn
+}
+
+public enum JSProgressHUDBackgroundStyle {
+    case solidColor
+    case blur
+}
+
