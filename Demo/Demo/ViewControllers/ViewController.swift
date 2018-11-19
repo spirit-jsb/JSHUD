287899dda1e1f4f59781fccf51a67f62061ba647
Max <sibo_jian_29903549@163.com>
2018-11-19 11:12:10 +0800

    [功能]($)
    初始化项目，初始化 JSProgressHUD Framework，初始化 JSProgressHUD-Demo。

---
+++ Demo/Demo/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  JSProgressHUD-Demo
+//
+//  Created by Max on 2018/11/19.
+//  Copyright © 2018 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: 生命周期
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
