60109dfcc5ad30c0f1a0cdf577fa5976c3621bc0
Max <sibo_jian_29903549@163.com>
2018-11-22 16:57:24 +0800

    [重构]($\JSRoundProgressView)
    重构 JSRoundProgressView，移除 `isAnnular` 属性，修改部分参数命名，调整部分方法。

--- Sources/Core/JSRoundProgressView.swift
+++ Sources/Core/JSRoundProgressView.swift
@@ -12,20 +12,15 @@ public class JSRoundProgressView: UIView {
 
     // MARK: 属性
     public var progress: CGFloat = 0.0 {
-        didSet {
+        willSet {
+            if newValue < 0.0 || newValue > 1.0 {
+                return
+            }
             self.setNeedsDisplay()
         }
     }
     
-    public var backgroundTintColor: UIColor = UIColor.white {
-        didSet {
-            if self.backgroundTintColor != oldValue {
-                self.setNeedsDisplay()
-            }
-        }
-    }
-    
-    public var progressTintColor: UIColor = UIColor.white {
+    public var progressTintColor: UIColor = UIColor.black {
         didSet {
             if self.progressTintColor != oldValue {
                 self.setNeedsDisplay()
@@ -33,8 +28,14 @@ public class JSRoundProgressView: UIView {
         }
     }
     
-    public var isAnnular: Bool = false
-    
+    public var trackTintColor: UIColor = UIColor.white {
+        didSet {
+            if self.trackTintColor != oldValue {
+                self.setNeedsDisplay()
+            }
+        }
+    }
+        
     // MARK: 初始化
     convenience init() {
         self.init(frame: CGRect(x: 0.0, y: 0.0, width: 37.0, height: 37.0))
@@ -49,84 +50,47 @@ public class JSRoundProgressView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public override func draw(_ rect: CGRect) {
-        let context = UIGraphicsGetCurrentContext()
-        
-        if self.isAnnular {
-            // Draw background
-            let lineWidth: CGFloat = 2.0
-            let backgroundPath = UIBezierPath()
-            
-            backgroundPath.lineWidth = lineWidth
-            backgroundPath.lineCapStyle = .butt
-            
-            let center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
-            let radius = (self.bounds.size.width - lineWidth) / 2.0
-            let startAngle: CGFloat = -(.pi / 2.0)
-            var endAngle = (2.0 * .pi) + startAngle
-            
-            backgroundPath.addArc(withCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-            
-            self.backgroundTintColor.set()
-            
-            backgroundPath.stroke()
-            
-            // Draw progress
-            let progressPath = UIBezierPath()
-            
-            progressPath.lineWidth = lineWidth
-            progressPath.lineCapStyle = .square
-            
-            endAngle = (self.progress * 2.0 * .pi) + startAngle
-            
-            progressPath.addArc(withCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-            
-            self.progressTintColor.set()
-            
-            progressPath.stroke()
-        }
-        else {
-            // Draw background
-            let lineWidth: CGFloat = 2.0
-            
-            let circleRect = self.bounds.insetBy(dx: lineWidth / 2.0, dy: lineWidth / 2.0)
-        
-            self.progressTintColor.setStroke()
-            self.backgroundTintColor.setFill()
-            
-            context?.setLineWidth(lineWidth)
-            context?.strokeEllipse(in: circleRect)
-
-            // Draw progress
-            let progressPath = UIBezierPath()
-            
-            progressPath.lineWidth = lineWidth * 2.0
-            progressPath.lineCapStyle = .butt
-            
-            let center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
-            let radius = (self.bounds.width / 2.0) - lineWidth
-            let startAngle: CGFloat = -(.pi / 2.0)
-            let endAngle = (self.progress * 2.0 * .pi) + startAngle
-            
-            progressPath.addArc(withCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-            
-            context?.setBlendMode(.copy)
-            
-            self.progressTintColor.set()
-            
-            progressPath.stroke()
-        }
-    }
-    
-    public override var intrinsicContentSize: CGSize {
-        return CGSize(width: 37.0, height: 37.0)
-    }
-    
     // MARK: 设置方法
     private func setupView() {
         self.backgroundColor = UIColor.clear
         self.isOpaque = false
     }
     
-    // MARK: 私有方法
+    // MARK: 重写父类方法
+    public override var intrinsicContentSize: CGSize {
+        return CGSize(width: 37.0, height: 37.0)
+    }
+    
+    public override func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+
+        let lineWidth: CGFloat = 2.0
+        let circleRect = self.bounds.insetBy(dx: lineWidth / 2.0, dy: lineWidth / 2.0)
+        
+        // Draw background
+        context?.setLineWidth(lineWidth)
+        context?.setStrokeColor(self.progressTintColor.cgColor)
+        context?.setFillColor(self.trackTintColor.cgColor)
+        
+        context?.strokeEllipse(in: circleRect)
+
+        // Draw progress
+        let progressPath = UIBezierPath()
+
+        progressPath.lineWidth = lineWidth * 2.0
+        progressPath.lineCapStyle = .butt
+        
+        let center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+        let radius = (self.bounds.width / 2.0) - lineWidth
+        let startAngle: CGFloat = -(.pi / 2.0)
+        let endAngle = (self.progress * 2.0 * .pi) + startAngle
+        
+        progressPath.addArc(withCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        
+        context?.setBlendMode(.copy)
+        
+        self.progressTintColor.set()
+        
+        progressPath.stroke()
+    }
 }
